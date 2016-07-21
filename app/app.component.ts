72c5634e4fbd5feec328caaeea664fad7b61bcf1
henley02 <410451447@qq.com>
2016-07-21 18:24:48 +0800

    commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import{HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <my-heroes></my-heroes>
+    `,
+    directives: [HeroesComponent],
+    providers: [
+        HeroService
+    ]
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
